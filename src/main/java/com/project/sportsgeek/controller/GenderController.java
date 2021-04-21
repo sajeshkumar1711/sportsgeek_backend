2e20bc6188b56e6d01ff6710432f957e8c81bd53
Suraj Gupta <surajgupta.work@gmail.com>
2021-04-21 20:33:17 +0530

    Added API Responses for controllers

--- src/main/java/com/project/sportsgeek/controller/GenderController.java
+++ src/main/java/com/project/sportsgeek/controller/GenderController.java
@@ -20,6 +20,9 @@ import com.project.sportsgeek.model.Gender;
 import com.project.sportsgeek.response.Result;
 import com.project.sportsgeek.service.GenderService;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping(path = "/genders",produces = MediaType.APPLICATION_JSON_VALUE)
 public class GenderController {
@@ -29,18 +32,20 @@ public class GenderController {
 
     @GetMapping
     @PreAuthorize("hasRole('Admin')")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success"), @ApiResponse(code = 500, message = "Internal server error"), @ApiResponse(code = 404, message = "Bad Request")})
     public ResponseEntity<Result<List<Gender>>> getAllGenders() {
         Result<List<Gender>> genderList = genderService.findAllGender();
         return new ResponseEntity<>(genderList, HttpStatus.valueOf(genderList.getCode()));
     }
     @GetMapping("/{id}")
     @PreAuthorize("hasAnyRole('Admin','User')")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success"), @ApiResponse(code = 500, message = "Internal server error"), @ApiResponse(code = 404, message = "Bad Request")})
     public ResponseEntity<Result<Gender>> getGenderById(@PathVariable int id) throws Exception {
         Result<Gender> genderList = genderService.findGenderById(id);
         return new ResponseEntity<>(genderList, HttpStatus.valueOf(genderList.getCode()));
     }
 
-    @PostMapping
+    @PostMapping("/add-gender")
     @PreAuthorize("hasAnyRole('Admin','User')")
     public ResponseEntity<Result<Gender>> addGender(@RequestBody(required = true) Gender gender) throws  Exception {
         Result<Gender> genderResult = genderService.addGender(gender);
@@ -48,12 +53,14 @@ public class GenderController {
     }
     @PutMapping("/{id}")
     @PreAuthorize("hasAnyRole('Admin','User')")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully updated schema"), @ApiResponse(code = 404, message = "Schema not found"), @ApiResponse(code = 400, message = "Missing or invalid request body"), @ApiResponse(code = 500, message = "Internal error")})
     public ResponseEntity<Result<Gender>> updateGender(@PathVariable int id,@RequestBody(required = true) Gender gender) throws Exception {
        Result<Gender> genderResult = genderService.updateGender(id,gender);
         return new ResponseEntity(genderResult,HttpStatus.valueOf(genderResult.getCode()));
     }
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('Admin')")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Successfully deleted schema"), @ApiResponse(code = 404, message = "Schema not found"), @ApiResponse(code = 409, message = "Schema is in use"), @ApiResponse(code = 500, message = "Error deleting schema")})
     public ResponseEntity<Result<Gender>> deleteGenderById(@PathVariable int id) throws Exception {
        Result<Integer> integerResult =  genderService.deleteGender(id);
         return new ResponseEntity(integerResult,HttpStatus.valueOf(integerResult.getCode()));
